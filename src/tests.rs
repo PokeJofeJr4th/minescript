9c667149d24d5b5e1836f71400987d74703468f9
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-06-20 14:32:01 -0400

    tested and fixed rational approximation

--- src/tests.rs
+++ src/tests.rs
@@ -292,3 +292,18 @@ mod interpreter {
         );
     }
 }
+
+mod types {
+    use crate::types::farey_approximation;
+
+    #[test]
+    fn rational_approximator() {
+        assert_eq!(farey_approximation(0.5, 10), (1, 2));
+        assert_eq!(farey_approximation(2.5, 10), (5, 2));
+        assert_eq!(farey_approximation(2.0, 10), (2, 1));
+        assert_eq!(farey_approximation(1.618, 10), (13, 8));
+        // examples from https://www.johndcook.com/blog/2010/10/20/best-rational-approximation/
+        assert_eq!(farey_approximation(0.367_879, 10), (3, 8));
+        assert_eq!(farey_approximation(0.367_879, 100), (32, 87));
+    }
+}
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -28,29 +28,54 @@ pub mod prelude {
     }
 
     /// Given a float, find the fraction with the closest value with a denominator less than a certain amount
+    #[allow(clippy::cast_possible_truncation)]
     pub fn farey_approximation(target: f32, max_denominator: i32) -> (i32, i32) {
-        let mut a = (0, 1);
-        let mut b = (1, 1);
+        let mut a = (target.floor() as i32, 1);
+        let mut b = (target.ceil() as i32, 1);
 
-        while b.1 <= max_denominator {
+        while b.1 < max_denominator && a.1 < max_denominator {
             let mediant_numerator = a.0 + b.0;
             let mediant_denominator = a.1 + b.1;
 
             let mediant_value = mediant_numerator as f32 / mediant_denominator as f32;
 
+            // println!("{a:?} < {mediant_value} < {b:?}");
+
             if (mediant_value - target).abs() < f32::EPSILON {
-                return (mediant_numerator, mediant_denominator);
+                println!("Returning early");
+                return simplify_fraction((mediant_numerator, mediant_denominator));
             } else if mediant_value < target {
                 a = (mediant_numerator, mediant_denominator);
             } else {
                 b = (mediant_numerator, mediant_denominator);
             }
+            // println!("{a:?} << {b:?}");
         }
+        // println!("Returning on time");
+        // println!("{a:?} << {b:?}");
+        // println!("{} > {max_denominator} || {} > {max_denominator}", a.1, b.1);
+        // make sure final answer is the closer one and inside the range
+        if (target - b.0 as f32 / b.1 as f32).abs() < (target - a.0 as f32 / a.1 as f32).abs()
+            && b.1 <= max_denominator
+        {
+            simplify_fraction(b)
+        } else if a.1 <= max_denominator {
+            simplify_fraction(a)
+        } else {
+            simplify_fraction(b)
+        }
+    }
 
-        if (target - a.0 as f32 / a.1 as f32).abs() < (target - b.0 as f32 / b.1 as f32).abs() {
+    fn simplify_fraction((numerator, denominator): (i32, i32)) -> (i32, i32) {
+        let gcd = euclidean_algorithm(numerator, denominator);
+        (numerator / gcd, denominator / gcd)
+    }
+
+    fn euclidean_algorithm(a: i32, b: i32) -> i32 {
+        if b == 0 {
             a
         } else {
-            b
+            euclidean_algorithm(b, a % b)
         }
     }
 }
