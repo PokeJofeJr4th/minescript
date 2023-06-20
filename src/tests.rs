0ab746a30ce2c28fb6568ff2f7630f3b1ee3bbb5
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-06-20 14:42:10 -0400

    aother rational_approximator test

--- src/tests.rs
+++ src/tests.rs
@@ -302,6 +302,7 @@ mod types {
         assert_eq!(farey_approximation(2.5, 10), (5, 2));
         assert_eq!(farey_approximation(2.0, 10), (2, 1));
         assert_eq!(farey_approximation(1.618, 10), (13, 8));
+        assert_eq!(farey_approximation(1.618, 100), (144, 89));
         // examples from https://www.johndcook.com/blog/2010/10/20/best-rational-approximation/
         assert_eq!(farey_approximation(0.367_879, 10), (3, 8));
         assert_eq!(farey_approximation(0.367_879, 100), (32, 87));
